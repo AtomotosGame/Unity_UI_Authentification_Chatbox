06c1b47a7623fbca03b73f93066671033c95c15d
Backendeng <backendeng0417@gmail.com>
2022-11-20 16:24:12 -0500

    Add chat box

---
+++ Client/Assets/Script/Chat/ChatInputBehavior.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChatInputBehavior : MonoBehaviour {
+
+	public ChatClientBehavior chatClient;
+	public string inputName { get; set; }
+	InputField inField;
+
+	void Start () {
+		inField = GetComponent<InputField>();
+	}
+	
+	// Note: End Edit event is called when losing focus (mouse clicks outside input field)
+	public void OnEndEdit(string s) {
+		if (s.Length < 1) // ignore empty strings
+			return;
+
+		if (chatClient.Send(inputName + ": " + s))
+			inField.text = "";
+			
+		// by default, Unity releases focus on the inputfield after pressing enter
+		inField.ActivateInputField(); // keep the focus
+	}
+}
