52582e196bd5161fcebcbbbd475174c39cf5bccd
Backendeng <backendeng0417@gmail.com>
2022-11-24 16:43:24 -0500

    Adding scroll view

---
+++ Client/Assets/Script/UI/TabScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject [] TabButtons;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
